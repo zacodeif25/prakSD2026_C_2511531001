f726d56ee9d5a165ca466917d1a93ad84fc7aa51
zacodeif25 <dzhillandzhalila028@gmail.com>
2026-09-16 13:21:40 +0700

    Delete prakalpro_2025_A_251151001 directory

--- prakalpro_2025_A_251151001/src/Pekan2/KelilingLingkaran.java
+++
@@ -1,9 +0,0 @@
-package Pekan2;
-
-public class KelilingLingkaran {
-	public static void main(String[] args) {
-		final double PI = 3.14; /*Definisi Konstanta*/
-		double radius = 100; /*Deklarasi Variabel*/
-		System.out.println("Keliling = " + 2 * PI * radius);
-	}
-}
